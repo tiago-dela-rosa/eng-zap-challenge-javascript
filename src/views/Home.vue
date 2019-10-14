0f75e91a17eefa227b0086b468f42497ef57cd43
Tiago <delarosalopes@gmail.com>
2019-10-14 17:39:03 -0300

    Adicionado Vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Filter />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Filter from '../components/Filter';
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
-  }
-}
+    Filter,
+  },
+};
 </script>
